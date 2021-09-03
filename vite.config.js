8c07bb19462b8c351b8e5a7b7b7eff8c263c87b4
diamant <git@diamant.dev>
2021-09-03 05:18:17 +0200

    ⚡️ PWA add support.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,40 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
-})
+  plugins: [
+    svelte(),
+    VitePWA({
+      includeAssets: ['robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Pokedaily',
+        short_name: 'What Pokémon are you today ?',
+        description: 'Telling You What Pokemon You Are Today',
+        theme_color: '#b69bc9',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+      workbox: {
+        cleanupOutdatedCaches: false,
+      },
+    }),
+  ],
+});
